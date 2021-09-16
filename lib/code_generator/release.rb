09429ef1ff779f7f8efa056cce74c49bd2d78795
Oleh Hudeichuk <emptystamp@gmail.com>
2021-09-16 06:52:26 +0300

    version 1.1.5

---
+++ lib/code_generator/release.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+script_file_path = File.expand_path(File.dirname(__FILE__))
+
+require "#{script_file_path}/api_converter.rb"
+require "#{script_file_path}/code_generator.rb"
+GEM_DIR = "#{script_file_path}/../.."
+
+
+`cd #{script_file_path} && curl https://raw.githubusercontent.com/tonlabs/TON-SDK/master/tools/api.json > api.json`
+api_json_path = "#{script_file_path}/../../api.json"
+json = ''
+if File.exists?(api_json_path)
+  json = File.read(api_json_path)
+else
+  p "File #{api_json_path} is not exist"
+  exit 0
+end
+converter = ApiConverter.new(json)
+types = converter.convert
+generator = CodeGenerator.new(types, GEM_DIR)
+generator.generate_self_code
+
+version_file = "#{script_file_path}/../ton-client-ruby/version.rb"
+file = File.read(version_file)
+
+p 'check version'
+if file[/VERSION = "(\d+)\.(\d+)\.(\d+)"/]
+  major = $1
+  minor = $2
+  current = $3
+  version = "#{major}.#{minor}.#{current.to_i + 1}"
+  p version
+  data = "module TonClient\n  VERSION = \"#{version}\"\nend\n\n"
+  p data
+  p version_file
+  File.open(version_file, 'wb') { |f| f.write(data) }
+  p 'update version'
+
+  puts "make release? Y/N"
+  option = gets
+  if option.strip.downcase == 'y'
+    system(%{cd #{script_file_path} && git add .})
+    system(%{cd #{script_file_path} && git commit -m 'version #{version}'})
+    system(%{cd #{script_file_path} && bash -lc 'rake release'})
+  end
+end
+
