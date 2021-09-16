2f47ba0284da94dbf7ea456e3ac9a3d4cb5edde6
Oleh Hudeichuk <emptystamp@gmail.com>
2021-09-16 05:10:01 +0300

    add version

--- lib/code_generator/code_generator.rb
+++ lib/code_generator/code_generator.rb
@@ -46,7 +46,8 @@ class CodeGenerator
     content = %{
 # Ruby Client for Free TON SDK
 
-[![GEM](https://img.shields.io/badge/ruby-gem-green)]()
+[![GEM](https://img.shields.io/badge/ruby-gem-orange)](https://rubygems.org/gems/ton-client-ruby)
+[![SPM](https://img.shields.io/badge/SDK%20VERSION-#{types.version}-green)](https://github.com/tonlabs/TON-SDK)
 
 ## Install
 
